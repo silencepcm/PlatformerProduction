c1b7db262ff307c3afc46ff19e14f5c8bd060376
Thomas L <seth.drakor@gmail.com>
2021-02-23 14:17:18 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovment : MonoBehaviour
+{
+
+    public PlayerController controller;
+    public float runSpeed = 40f;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+       //animator.SetFloat("PlayerSpeed", Mathf.Abs(horizontalMove));
+
+
+       if (Input.GetButtonDown("Jump")){
+           jump = true;
+       }
+    }
+
+    void FixedUpdate() {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+
+}
