24374a7222c4e589fd98577f57fdee5f634fbc99
silencepcm <igor.kiselev090@gmail.com>
2021-03-01 05:23:20 +0300

    Added HealthScript and damage from thorns

---
+++ Assets/Scripts/PersonnagesScripts/PlayerScripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Vector2 offset;
+    private Transform playerTransform;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+        temp.x += offset.x;
+        temp.y += offset.y;
+        transform.position = temp;
+    }
+}
